79cf98306669c55f2e3a8f83f42f918bcf2521ff
KrokodilGena07 <pasha.fenev@gmail.com>
2024-09-02 00:34:24 +0300

    set up server

---
+++ server/error/ApiError.js
@@ -0,0 +1,17 @@
+class ApiError extends Error {
+    constructor(status, message, errors) {
+        super(message);
+        this.status = status;
+        this.errors = errors;
+    }
+
+    static badRequest(message, errors) {
+        return new ApiError(400, message, errors);
+    }
+
+    static unauthorized() {
+        return new ApiError(401, 'unauthorized error');
+    }
+}
+
+module.exports = ApiError;
---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+require('dotenv/config');
+const router = require('./router/index');
+const errorMiddleware = require('./middlewares/errorMiddleware');
+const db = require('./config/db');
+const compression = require('compression');
+const compressionMiddleware = require('./middlewares/compressionMiddleware');
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(fileUpload({}));
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}));
+app.use(compression({filter: compressionMiddleware}));
+app.use('/api', router);
+app.use(errorMiddleware);
+
+async function start() {
+    try {
+        app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+        await db.authenticate();
+        await db.sync();
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start().then();
